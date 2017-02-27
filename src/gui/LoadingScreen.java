bb19a2f3b85e38afe85938588aa9b655ae9f70b4
kirjason <krjason.choi@gmail.com>
2017-02-27 13:52:01 -0800

    Update LoadingScreen.java

--- src/gui/LoadingScreen.java
+++ src/gui/LoadingScreen.java
@@ -11,13 +11,16 @@ import gui.tools.ImageDisplay;
 /**
  * The loading screen that is displayed when the application is first booted up.
  * 
- * @author Jarred
- * @version 2/13/2017
+ * @author Jarred, Jason
+ * @version 2/27/2017
  * @since 1/14/2017
  */
 public class LoadingScreen extends JPanel {
 	private static final long serialVersionUID = 1L;
-
+	/**
+	 * Constructor for LoadingScreen
+	 * Adds necessary visual variables
+	 */
 	public LoadingScreen() {
 		setLayout(new BorderLayout());
 		add(new ImageDisplay("res/img/GroupLogo.png"), BorderLayout.CENTER);
@@ -25,6 +28,9 @@ public class LoadingScreen extends JPanel {
 	}
 	
 	@Override
+	/**
+	 * @inheritDoc
+	 */
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 	}
