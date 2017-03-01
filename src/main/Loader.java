6445543288708b6a79b57d2f93743f49c86a3fcb
kirjason <krjason.choi@gmail.com>
2017-02-28 18:12:02 -0800

    Update Loader.java

--- src/main/Loader.java
+++ src/main/Loader.java
@@ -6,8 +6,8 @@ import web.LoadCredentialsFromFile;
 /**
  * A class that handles loading the required data and then stores it for parts of the program to recall later.
  * 
- * @author Jarred
- * @version 1/15/2017
+ * @author Jarred, Jason
+ * @version 2/28/2017
  * @since 1/14/2017
  */
 public class Loader implements Runnable{
@@ -16,11 +16,17 @@ public class Loader implements Runnable{
 	
 	public boolean hasCredentials;
 	
+	/**
+	 * Constructor for Loader 
+	 */
 	public Loader() {
 		isLoaded=false;
 		hasCredentials=false;
 	}
 	
+	/**
+	 * @inheritDoc
+	 */
 	@Override
 	public void run() {
 		if(LoadCredentialsFromFile.hasLoginCredentials()) {
@@ -34,7 +40,10 @@ public class Loader implements Runnable{
 		}
 		DebugLog.logStatement("Main has been woken up. (concurrency)", DebugLog.CONCURRENCY_LOG_CODE);
 	}
-
+	
+	/**
+	 * returns current status of isLoaded
+	 */
 	public boolean isLoaded() {
 		return isLoaded;
 	}
